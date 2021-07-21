149a47c22c24ecfda0f9ec4bbc65d6da733759bd
Admin <Admin@DESKTOP-EISFHQ1>
2021-07-21 20:14:08 +0530

    api trial commit

---
+++ API Trials/src/API_Trials/codePiece.java
@@ -0,0 +1,28 @@
+package API_Trials;
+
+public class codePiece {
+	
+	static int a =10;
+	final int b = 12;
+	
+	public void intChange(int i)
+	{
+		System.out.println("int is: "+a);
+		a=i;
+		//b = 13;
+		System.out.println("int is: "+a);
+		codePiece cp1 = new codePiece();
+		cp1.a=20;
+		System.out.println("int is: "+cp1.a);
+		codePiece.a=14;
+		System.out.println("int is: "+a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		codePiece cp = new codePiece();
+		cp.intChange(8);
+		System.out.println("int is: "+cp.a);
+	}
+
+}
