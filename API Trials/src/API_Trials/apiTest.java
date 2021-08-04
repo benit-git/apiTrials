76e8946d6ae87b0c9047eb1ac13133b9b38e3ad5
BenitS <BenitS@IMCLBCP23-MSL2.mumbai1.corp.citiustech.com>
2021-08-04 17:53:35 +0530

    added the authKey variable

--- API Trials/src/API_Trials/apiTest.java
+++ API Trials/src/API_Trials/apiTest.java
@@ -147,7 +147,7 @@ public class apiTest {
 	public void Auth_post_Test()
 	{
 		System.out.println("This method is for authentication calls");	
-		
+		String authKey = "";
 	}
 
 	public static void main(String[] args) throws InterruptedException {
